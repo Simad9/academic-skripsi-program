bdd1e864e0e95b2f5ac78ceafdc2c69ef28894a3
Wijdan <wijdanbita@gmail.com>
2025-12-05 10:16:28 +0700

    update : slicing section input dataset

--- nocd/main/views.py
+++ nocd/main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, "index.html")
+  number_list = range(1, 30)
+  context = {
+        'numbers': number_list,
+    }
+
+  return render(request, "index.html", context)
